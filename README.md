c02ebfad026b820df18cbd53f711684bfdf4c1bb
Arne Brasseur <arne@arnebrasseur.net>
2021-04-05 09:43:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# lambdaisland/dom-types
+
+<!-- badges -->
+[![CircleCI](https://circleci.com/gh/lambdaisland/dom-types.svg?style=svg)](https://circleci.com/gh/lambdaisland/dom-types) [![cljdoc badge](https://cljdoc.org/badge/lambdaisland/dom-types)](https://cljdoc.org/d/lambdaisland/dom-types) [![Clojars Project](https://img.shields.io/clojars/v/lambdaisland/dom-types.svg)](https://clojars.org/lambdaisland/dom-types)
+<!-- /badges -->
+
+Implement ClojureScript print handlers, as well Datify/Navigable for various
+built-in browser types.
+
+Work in progress.
+
+## License
+
+Copyright &copy; 2021 Arne Brasseur and Contributors
+
+Licensed under the term of the Mozilla Public License 2.0, see LICENSE.
+
+Available under the terms of the Eclipse Public License 1.0, see LICENSE.txt
---
+++ src/lambdaisland/dom_types.cljs
@@ -0,0 +1,94 @@
+(ns lambdaisland.dom-types
+  "Extend Pixi and DOM types with ClojureScript protocols."
+  (:require [applied-science.js-interop :as j]
+            [camel-snake-kebab.core :as csk]
+            [clojure.core.protocols :as protocols]
+            [clojure.string :as str]
+            [lambdaisland.data-printers :as data-printers]))
+
+(defn register-printer [type tag to-edn]
+  (data-printers/register-print type tag to-edn)
+  (data-printers/register-pprint type tag to-edn))
+
+(defn register-keys-printer [type tag keys]
+  (register-printer type tag (fn [obj]
+                               (reduce (fn [m k]
+                                         (assoc m k (j/get obj k)))
+                                       {}
+                                       keys)))
+  (extend-type type
+    protocols/Datafiable
+    (datafy [obj]
+      (into ^{:type type} {} (map (juxt identity #(j/get obj %))) keys))
+    protocols/Navigable
+    (nav [obj k v]
+      (j/get obj k))))
+
+(def ^:dynamic *max-attr-length* 250)
+
+(defn- truncate-attr [attr-val]
+  (if (and (string? attr-val) (< *max-attr-length* (.-length attr-val)))
+    (str (subs attr-val 0 *max-attr-length*) "...")
+    attr-val))
+
+(register-printer js/Text js/Text #(.-data %))
+
+(register-printer
+ js/Element
+ 'js/Element (fn hiccupize [^js e]
+               (cond
+                 (string? e)
+                 e
+                 (instance? js/Text e)
+                 (.-data e)
+                 :else
+                 (let [el [(keyword (str/lower-case (.-tagName e)))]]
+                   (into (if-let [attrs (seq (.getAttributeNames e))]
+                           (conj el (into {}
+                                          (map (juxt csk/->kebab-case-keyword
+                                                     #(truncate-attr (.getAttribute e %))))
+                                          attrs))
+                           el)
+                         (map hiccupize)
+                         (.-childNodes e))))))
+
+(register-printer js/HTMLDocument 'js/HTMLDocument (fn [^js d] {:root (.-documentElement d)}))
+(register-printer js/XMLDocument 'js/XMLDocument (fn [^js d] {:root (.-documentElement d)}))
+(register-printer js/Document 'js/Document (fn [^js d] {:root (.-documentElement d)}))
+
+(extend-type js/Node
+  ITransientCollection
+  (-conj! [^js this child]
+    (.appendChild this child)))
+
+(register-keys-printer js/Window 'js/Window [:location :document :devicePixelRatio :innerWidth :innerHeight])
+
+(register-printer js/Location 'js/Location str)
+
+(register-printer js/HTMLCollection 'js/HTMLCollection #(into [] %))
+(register-printer js/NodeList 'js/NodeList #(into [] %))
+
+
+(when (exists? js/KeyboardEvent)
+  (register-keys-printer js/KeyboardEvent 'js/KeyboardEvent [:type :code :key :ctrlKey :altKey :metaKey :shiftKey :isComposing :location :repeat]))
+
+(when (exists? js/TouchEvent)
+  (register-keys-printer js/TouchEvent 'js/TouchEvent [:altKey :changedTouches :ctrlKey :metaKey :shiftKey :targetTouches :touches])
+  (register-keys-printer js/Touch 'js/Touch [:identifier :screenX :screenY :clientX :clientY :pageX :pageY :target])
+  (register-printer js/TouchList 'js/TouchList (comp vec seq))
+  (lookupify js/TouchEvent)
+  (lookupify js/Touch)
+  (lookupify js/TouchList))
+
+(register-keys-printer js/PointerEvent 'js/PointerEvent [:pointerId :width :height :pressure
+                                                         :tangentialPressure :tiltX :tiltY
+                                                         :twist :pointerType :isPrimary])
+
+(def mouse-event-keys [:altKey :button :buttons :clientX :clientY :ctrlKey :metaKey :movementX :movementY
+                       ;; "These are experimental APIs that should not be used in production code" -- MDN
+                       ;; :offsetX :offsetY :pageX :pageY
+                       :region :relatedTarget :screenX :screenY :shiftKey])
+
+(register-keys-printer js/MouseEvent 'js/MouseEvent mouse-event-keys)
+(register-keys-printer js/WheelEvent 'js/WheelEvent (conj mouse-event-keys :deltaX :deltaY :deltaZ :deltaMode))
+(register-keys-printer js/DragEvent 'js/DragEvent (conj mouse-event-keys :dataTransfer))
